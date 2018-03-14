d76b1ba21a41a9a2574b0033bc5ade4071362cb7
taufanmahaputra <taufanmhptr@gmail.com>
2018-03-14 16:05:43 +0700

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public PlayerController player;
+	public bool isFollowing;
+
+	public float xOffset;
+	public float yOffset;
+
+	public float positionY;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<PlayerController> ();
+
+		isFollowing = true;
+		positionY = transform.position.y + yOffset;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isFollowing)
+			transform.position = new Vector3 (player.transform.position.x + xOffset, positionY, transform.position.z);
+	}
+}
