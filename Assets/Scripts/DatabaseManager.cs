4698abc1a7af520ebe4594c7a220a51c9a23ced8
Abdurrahman <13515024@std.stei.itb.ac.id>
2018-03-22 01:38:20 +0700

    done reading from sqlite db

---
+++ Assets/Scripts/DatabaseManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Data;
+using Mono.Data.Sqlite;
+
+public class DatabaseManager : MonoBehaviour {
+
+    private string connectionString;
+
+	// Use this for initialization
+	void Start () {
+        connectionString = "URI=file:" + Application.dataPath + "/Database/score.db";
+        GetScores();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void GetScores()
+    {
+        using (IDbConnection dbConnection = new SqliteConnection(connectionString))
+        {
+            dbConnection.Open();
+
+            using(IDbCommand dbCmd = dbConnection.CreateCommand())
+            {
+                string sqlQuery = "select * from playerScore";
+
+                dbCmd.CommandText = sqlQuery;
+
+                using(IDataReader reader = dbCmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Debug.Log(reader.GetValue(0) + " " + reader.GetValue(1));
+                    }
+                    reader.Close();
+
+                }
+                
+            }
+            dbConnection.Close();
+        }
+    }
+}
