2b40de1269280b6726e33bde559ebb91739bb57c
taufanmahaputra <taufanmhptr@gmail.com>
2018-03-21 20:37:23 +0700

    add coin pickup to reduce score time

---
+++ Assets/Scripts/CoinPickups.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickups : MonoBehaviour {
+	public int reduceTime;
+
+	void OnTriggerEnter2D (Collider2D thing) {
+		if (thing.GetComponent<PlayerController>() == null) {
+			return;
+		}
+
+		ScoreManager.reduceTime(reduceTime);
+
+		Destroy(gameObject);
+	}
+}
