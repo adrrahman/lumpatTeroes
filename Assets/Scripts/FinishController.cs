c4a86f034ce6cbd36b8a17bae0bc61aec03553fa
taufanmahaputra <taufanmhptr@gmail.com>
2018-03-21 21:12:55 +0700

    add finish flag

---
+++ Assets/Scripts/FinishController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishController : MonoBehaviour {
+	private Scene scene;
+	// Use this for initialization
+	void Start () {
+		scene = SceneManager.GetActiveScene();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D thing) {
+		string sceneName = scene.name;
+		Debug.Log (sceneName);
+
+		if (sceneName == "Scene1") {
+			Application.LoadLevel ("Scene2");
+		}
+	}
+}
