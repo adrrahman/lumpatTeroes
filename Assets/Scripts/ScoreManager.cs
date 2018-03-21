840f352e7175fdc10386dfd344ce49d17670f009
taufanmahaputra <taufanmhptr@gmail.com>
2018-03-21 19:54:59 +0700

    add score manager and background music

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+	public static float scoreTime;
+
+	Text text;
+
+	void Start() {
+		text = GetComponent<Text> ();
+
+		scoreTime = 0;
+	}
+
+	void Update() {
+		if (scoreTime < 0)
+			scoreTime = 0;
+
+		text.text = scoreTime.ToString("#.00");
+		scoreTime += Time.deltaTime;
+
+	}
+
+	public static void reduceTime( float timeToReduce) {
+		scoreTime -= timeToReduce;
+	}
+
+	public static void reset() {
+		scoreTime = 0;
+	}
+}
